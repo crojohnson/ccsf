d785af13ca704fb6c6367cfd42463b98cae6178b
Greg Gorlen <gsgorlen@gmail.com>
2016-09-09 21:48:21 -0700

    Update c2i.py

--- c2i.py
+++ c2i.py
@@ -1,44 +1,33 @@
 """
-This program formats code for CCSF Insight's "moodle autoformat option" and copies it to clipboard.
+This program formats code for CCSF Insight's 
+"moodle autoformat option" and copies it to clipboard.
 Requires Pyperclip
 """
 
+import argparse
 from sys import argv
 from pyperclip import copy
 
-java_keywords = ["abstract", "continue", "for", "new", "switch",
-                "assert", "default", "goto", "package", "synchronized",
-                "boolean", "do", "if", "private", "this", "break",
-                "double", "implements", "protected", "throw", "byte",
-                "else", "import", "public", "throws", "case", "enum",
-                "instanceof", "return", "transient", "catch", "extends",
-                "int", "short", "try", "char", "final", "interface", "static", 
-                "void", "class", "finally", "long", "strictfp", "volatile",
-                "const", "float", "native", "super", "while"]
+parser.add_argument("-i", help = "Input file", required = True)
+parser.add_argument("-o", help = "Output file", required = False)
 
-if len(argv) in [2, 3]:
-    with open(argv[1]) as file:
-        result = "<font style=\"font-family:'Courier New';font-size:10pt;\">"
+args = vars(parser.parse_args())
+
+with open(args.get("i")) as file:
+    result = "<font style=\"font-family:'Courier New';font-size:10pt;\">"
+
+    for line in file:
+
+        result +=  line.replace("<", "&lt;").replace(">", "&gt;")\
+                   .replace("\t", "    ").replace(" ", "&nbsp;")
+                  
+    result += "</font>" 
     
-        for line in file:
-            result += line.replace(" ", "&nbsp;").replace("<", "&lt;").replace(">", "&gt;")
+# Copy result to clipboard
+copy(result)
 
-        # todo: implement syntax highlighting
-        #for keyword in java_keywords:
-        #    result = result.replace(keyword + '&nbsp;', "<font style='color:blue;'>" + keyword + "</font>&nbsp;")
-
-
-    # copy result to clipboard
-    copy(result)
-
-    # if filename was provided, write result to file
-    if len(argv) == 3:
-        f = open(argv[2], 'w')
-        f.write(result)
-        f.close()
-    
-else:
-    print("\nUsage: python c2i.py InputFilename [OutputFilename]\n\nWarning: running " +
-    "this program will overwrite your clipboard contents.\nRequires pyperclip.  " +
-    "To install it, run: pip install Pyperclip\n")
-    
+# If filename was provided, write result to file
+if args.get("o"):
+    f = open(args.get("o"), 'w')
+    f.write(result)
+    f.close()
